fee041d3181f76ecaf7bc69c783f22df9070e6dd
aevenko <a.evenko@gmail.com>
2025-05-10 15:55:55 -0400

    Add files via upload

---
+++ ratings.js
@@ -0,0 +1,4 @@
+
+document.getElementById("fqe-rating").textContent = "1180";
+document.getElementById("cfc-rating").textContent = "1500";
+document.getElementById("fide-rating").textContent = "Unrated";
